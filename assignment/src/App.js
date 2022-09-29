e15ffa198bc930a77d1ef0e40f1485974557547a
iantom12 <109991370+iantom12@users.noreply.github.com>
2022-09-29 16:03:51 -0400

    Assignment 3

---
+++ assignment/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from './Components/Footer.js';
+import Form from './Components/Form.js';
+import Header from './Components/Header.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Form/>
+      <br></br>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
